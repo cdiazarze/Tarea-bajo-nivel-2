d92f3bfa4943b43650d935b7580cd401624ff695
cdiazarze <cdiaz1@miuandes.cl>
2018-04-24 22:39:25 -0300

    To File Complete

--- Analyzer.c
+++ Analyzer.c
@@ -4,7 +4,16 @@
 #include "estructura.h"
 #define split ' '
 #define FILE_LENGTH 30
-
+#define colpj 1
+#define colpt 2 
+#define colpg 3
+#define colpe 4
+#define coldg 5
+#define colgf 6
+#define colgv 7
+#define colpp 8
+#define colgc 9
+#define colgl 10
 
 /*void cifrado_desplazamiento(int numero, char *linea)
 {
@@ -67,6 +76,7 @@ void SetRows(char *fileName, int *dim){
  			dim[0]+=1;
 		}
 	}
+	dim[0]+=1;
 	fclose(in);
 }
 
@@ -78,20 +88,21 @@ void SetColumns(char *fileName, int *dim){
 	while((c=fgetc(in))!='\n'){
 		if (c==split) dim[1]+=1;
 	}
-	dim[1]+=1;
+	dim[1]+=4;
 	fclose(in);
 }
 
 
-void CreateArray(char *fileName, char ***dataMatrix){
+void CreateArray(char *fileName, char ***dataMatrix,int *dim){
 	
 	char c;
 	FILE *in;
-	int i=0;
+	int i=1;
 	int j=0;
 	int z=0;
 	int size=0;
 	in=fopen(fileName,"r");
+	SetColumnNames(dataMatrix, dim);
 	while ((c=fgetc(in))!=EOF){
 		if (c!=split && c!='\n'){
  			dataMatrix[i][j][z]=c;
@@ -105,11 +116,34 @@ void CreateArray(char *fileName, char ***dataMatrix){
 			j+=1;
 		}
 		if (c=='\n'){
+			sprintf(dataMatrix[i][colpp], "%d", atoi(dataMatrix[i][colpj])-atoi(dataMatrix[i][colpg])-atoi(dataMatrix[i][colpe]));
+			sprintf(dataMatrix[i][colgc], "%d", atoi(dataMatrix[i][colgf])-atoi(dataMatrix[i][coldg]));
+			sprintf(dataMatrix[i][colgl], "%d", atoi(dataMatrix[i][colgf])-atoi(dataMatrix[i][colgv]));
 			z=0;
 			j=0;
 			i+=1;
 		}	
-	}		
+	}
+	fclose(in);
+			
+}
+
+void SetColumnNames(char ***dataMatrix,int *dim){
+	char columnName[11][7]={"Equipo","pj", "puntos", "pg", "pe", "dg", "gf","gv", "pp", "gc", "gl"};
+	for (int i=0; i<dim[1]; i++) for (int j=0; j<(int)strlen(columnName[i])+1; j++) dataMatrix[0][i][j]=columnName[i][j];
+}
+
+void w(char ***dataMatrix, char *outfileName, int *dim){
+	FILE *out;
+	if (outfileName[0]!=0) out=fopen(outfileName,"w");
+	else out=stdout;
+	for (int i=0; i<dim[0]; i++){
+		for (int j=0; j<dim[1]; j++){
+ 			fprintf(out, "%15s", dataMatrix[i][j]);
+			if (j<dim[1]-1) fprintf(out, "%c", ' ');
+			else fprintf(out, "%c", '\n');
+		}	 	
+	}
 }
 
 
@@ -119,10 +153,9 @@ int main(int argc,char **argv)
 	//int (*ff[])(int, int) = {-w};
 	int nf0=2;
 	void (*ff0[])(char*, int*) = {SetRows,SetColumns};
-	FILE *out;
 	char *fileName = malloc(FILE_LENGTH*sizeof(char));
 	int dim[2]={0};
-	 
+	char *outfileName = calloc(FILE_LENGTH,sizeof(char)); 
 
 	
 
@@ -130,26 +163,16 @@ int main(int argc,char **argv)
 	fgets(fileName, FILE_LENGTH-1, stdin);
 	if (!CheckFileExist(fileName)) return 1;
 	for (int i=0; i<nf0;i++) ff0[i](fileName,dim);
-	fprintf(stderr, "%d\n", dim[0]);
-	fprintf(stderr, "%d\n", dim[1]);
-	
+
 	//Incializar arreglo de datos
 	char *** dataMatrix=malloc(dim[0]*dim[1]*FILE_LENGTH*sizeof(char));
 	for (int i=0; i<dim[0]; i++){
 		dataMatrix[i]=malloc(dim[1]*FILE_LENGTH*sizeof(char));
 		for (int j=0; j<dim[1];j++) dataMatrix[i][j]=malloc(FILE_LENGTH*sizeof(char));
  	}
-	CreateArray(fileName, dataMatrix);
-
-
-	if (argc>1) out=fopen(argv[1],"w");	
-	
-	for (int i=0; i<dim[0]; i++){
-	 for (int j=0; j<dim[1]; j++) printf("%s,",dataMatrix[i][j]);
-	 printf("%c",'\n');	
-	}
-	fprintf(out, "Lineas: %d\n", dim[0]);
-	fprintf(out, "Largo: %d\n", dim[1]);
+	CreateArray(fileName, dataMatrix,dim);
+	if (argc>2) w(dataMatrix,argv[1],dim);
+	else w(dataMatrix,outfileName,dim);
 
 	free(fileName);
 	free(dataMatrix);
--- estructura.h
+++ estructura.h
@@ -6,6 +6,8 @@
 int CheckFileExist(char *fileName);
 void SetRows(char *fileName, int *dim);
 void SetColumns(char *fileName, int *dim);
-void CreateArray(char *fileName, char ***dataMatrix);
+void CreateArray(char *fileName, char ***dataMatrix, int *dim);
+void SetColumnNames(char ***dataMatrix,int *dim);
+void w(char ***dataMatrix, char *outfileName, int *dim);
 
 #endif
